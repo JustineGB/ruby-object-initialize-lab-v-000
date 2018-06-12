1591aa5e05750596332c248aae63026a26d75c7c
Justine Bricknell <justinegayton@gmail.com>
2018-06-12 22:33:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,6 @@
 class Dog
-  
-def initialize(name)
+
+def initialize(name, breed = "Mutt")
  @name = name
+ @breed = breed
 end
-
-def name=(name)
- @name = name
-	end
-
-def name
-	  @name
-	end
-
-  def initialize(breed)
-   @breed = breed
-  end
-
-  def breed=(breed)
-   @breed = breed
-  	end
-
-  def breed
-  	  @breed
-  	end
-  end
